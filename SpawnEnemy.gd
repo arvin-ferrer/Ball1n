de6b0ca148e0b19e3d6e46af1ac8796ec543306c
RCubed <126275053+Xertreb@users.noreply.github.com>
2025-11-22 12:02:04 +0800

    feat: added random spawn features

---
+++ SpawnEnemy.gd
@@ -0,0 +1,19 @@
+extends Path2D
+
+@export var spawn_rate : float = 2.0
+@export var enemy : PackedScene
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	$SpawnTimer.start(spawn_rate)
+
+func _on_spawn_timer_timeout() -> void:
+	var percent_position = rng.randf_range(0.0, 1.0);
+	var pos = curve.sample_baked(percent_position*curve.get_baked_length());
+	
+	print("spawn at: ")
+	print(pos)
+	
+	var enemy_instance = enemy.instantiate()
+	enemy_instance.position = pos
+	get_parent().add_child(enemy_instance)
