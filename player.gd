928a66401010422abd9b77c3859e3dd1f68cdea6
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2025-11-22 09:19:16 +0800

    feat: add firing mechanism

--- player.gd
+++ player.gd
@@ -2,7 +2,9 @@ extends Area2D
 signal hit
 
 @export var speed = 400
+@export var bullet_scene: PackedScene
 var screen_size
+var has_bullet = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,7 +33,14 @@ func _process(delta):
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO,screen_size)
-	
+	# shooting logic
+	if Input.is_action_just_pressed("shoot"):
+		print("button pressed") 
+		if has_bullet:
+			print("have bullet") 
+			fire_bullet()
+		else:
+			print("no bullet")
 	#if velocity.x != 0:
 		#$AnimatedSprite2D.animation = "walk"
 		#$AnimatedSprite2D.flip_v = false
@@ -40,7 +49,23 @@ func _process(delta):
 		#$AnimatedSprite2D.animation = "up"
 		#$AnimatedSprite2D.flip_v = velocity.y > 0
 
+func fire_bullet():
+	if bullet_scene == null:
+		print("ERROR: No Bullet Scene assigned in Inspector!")
+		return
 
+	var b = bullet_scene.instantiate()
+	
+	b.global_position = $Muzzle.global_position
+	b.rotation = rotation  
+	
+	get_tree().root.add_child(b)
+	
+	has_bullet = false
+
+func reload():
+	has_bullet = true
+	
 func _on_body_entered(body):
 	hide()
 	hit.emit()
