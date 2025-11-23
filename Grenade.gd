af2fd36137e0b2cd78dff5edb86f78a311a32937
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2025-11-23 21:58:07 +0800

    chore: change bullet sprite to fireball

--- Grenade.gd
+++ Grenade.gd
@@ -30,20 +30,15 @@ func explode():
 	await get_tree().create_timer(0.6).timeout
 	queue_free()
 func spawn_explosion_particles():
-	# Check if the node exists
+
 	if has_node("CPUParticles2D"):
 		var particles = $CPUParticles2D
-		
 	
 		remove_child(particles)
 		get_tree().root.add_child(particles)
 		
-		
 		particles.global_position = global_position
-		
-		
 		particles.emitting = true
-		
 	
 		await get_tree().create_timer(1.0).timeout
 		particles.queue_free()
